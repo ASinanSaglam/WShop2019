ae741ad8066a1c2ed7faf997f8d26e5483f96b4b
Ali Sinan Saglam <asinansaglam@gmail.com>
2019-04-25 19:31:12 +0000

    Removed a couple extra examples, ran the rest

--- run_sim.sh
+++ run_sim.sh
@@ -2,10 +2,10 @@ source /home/test/mcell/mcell_venv/bin/activate
 MODEL=$1
 NAME=$(basename $MODEL|sed 's/.mdlr//')
 if [ -d run ];then
-  rm -rf run
+  rm -rf $NAME
 fi
-mkdir run
-cp $MODEL run/.
-cp icogeometry.mdl run/.
-cd run
+mkdir $NAME
+cp $MODEL $NAME/.
+cp icogeometry.mdl $NAME/.
+cd $NAME
 python3 /home/test/mcell/build/mdlr2mdl.py -ni $MODEL -o $NAME -r || exit 1 
